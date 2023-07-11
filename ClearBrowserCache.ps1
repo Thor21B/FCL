e07d4bde201314da273a87437c7f8bc2da32786a
Thor21B <137902330+Thor21B@users.noreply.github.com>
2023-07-11 13:59:05 +0100

    Update ClearBrowserCache.ps1
    added clearing of ms edge

--- ClearBrowserCache.ps1
+++ ClearBrowserCache.ps1
@@ -1,4 +1,6 @@
-param([string] $username="")  
+param([string] $username=(whoami).Split('\')[1])  
+
+Write-host "Username is $username"
 
 if(Test-Path -Path C:\Users\$username\AppData\Local\Google\Chrome\User` Data\Default\Cache)  
 
@@ -32,6 +34,42 @@ else
 
 {  
 
-   Write-host "Specified path doesn't exist."  
+   Write-host "Specified path doesn't exist for Chrome Cache [C:\Users\$username\AppData\Local\Google\Chrome\User` Data\Default\Cache]"  
 
+} 
+
+if(Test-Path -Path C:\Users\$username\AppData\Local\Microsoft\Edge\User` Data\Default\Cache)  
+ 
+{  
+ 
+   $itemsCountBefore = Get-ChildItem -Path C:\Users\$username\AppData\Local\Microsoft\Edge\User` Data\Default\Cache -Recurse | Measure-Object | %{$_.Count} 
+ 
+   Remove-Item -Path C:\Users\$username\AppData\Local\Microsoft\Edge\User` Data\Default\Cache -Recurse -Force  
+ 
+   $itemsCountAfter = Get-ChildItem -Path C:\Users\$username\AppData\Local\Microsoft\Edge\User` Data\Default\Cache -Recurse | Measure-Object | %{$_.Count} 
+ 
+   if($itemsCountBefore -gt $itemsCountAfter) 
+ 
+   { 
+ 
+      Write-host "Cache cleared for Microsoft Edge" 
+ 
+   } 
+ 
+   else 
+ 
+   { 
+ 
+      Write-Host "Failed to clear cache for Microsoft Edge" 
+ 
+   } 
+ 
+}  
+ 
+else  
+ 
+{  
+ 
+   Write-host "Specified path doesn't exist for Microsoft Edge cache [C:\Users\$username\AppData\Local\Microsoft\Edge\User` Data\Default\Cache]"  
+ 
 } 
